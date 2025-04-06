df9141fd0aa6ac5b568d8a38f115a9c12a900c42
Khelan Mehta <khelan05@gmail.com>
2025-04-06 11:30:51 +0530

    pushing

--- src/s3.ts
+++ src/s3.ts
@@ -1,13 +1,1 @@
-import aws from 'aws-sdk';
 
-const region = "eu-north-1";
-const bucketName = "direct-upload-s3-khelan";
-const accessKeyId = "AKIAVVANBAJNCJ3LVIJC";
-const secretAccessKey = "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";
-
-const s3 = new aws.S3({
-    region,
-    accessKeyId,
-    secretAccessKey,
-    signatureVersion: '4'
-});
